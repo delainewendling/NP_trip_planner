f63596b0d34d6ae99c7e0f527174c4786ecc01ac
Delaine Wendling <delaine.wendling@gmail.com>
2016-09-07 15:51:06 -0500

    Got map to show up in the correct area with Yosemite as the center

--- app/controllers/exploreCtrl.js
+++ app/controllers/exploreCtrl.js
@@ -3,8 +3,8 @@
 app.controller("ExploreCtrl", function($scope, ImportantKeys, uiGmapIsReady, uiGmapGoogleMapApi){
   //Will add the map view here
     $scope.map = {
-      center: {latitude: 40.7128, longitude: -74.0059 },
-      zoom: 14 };
+      center: {latitude: 37.8651, longitude: -119.5383 },
+      zoom: 9 };
     $scope.options = {scrollwheel: false};
     $scope.markers = [];
   // uiGmapGoogleMapApi is a promise.
