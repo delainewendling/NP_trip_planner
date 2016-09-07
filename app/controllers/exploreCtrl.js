b5dc6f8c86a2381f8c6a8d53586e24200ff20d9e
Delaine Wendling <delaine.wendling@gmail.com>
2016-09-07 12:02:15 -0500

    Added google maps to explore controller

--- app/controllers/exploreCtrl.js
+++ app/controllers/exploreCtrl.js
@@ -1,5 +1,9 @@
 "use strict";
 
 app.controller("ExploreCtrl", function($scope){
-  
-})
+  //Will add the map view here
+  $scope.initMap = function(){
+    $scope.map = new google.maps.Map(document.getElementById('map'), )
+  };
+
+});
