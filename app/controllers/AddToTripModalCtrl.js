7e5d8bbd4ab167039985b950f3a16bd358e0fb55
Delaine Wendling <delaine.wendling@gmail.com>
2016-09-13 18:51:44 -0500

    Added a modal that prompts user to choose a trip, day, and time to complete a trail

--- app/controllers/AddToTripModalCtrl.js
+++ app/controllers/AddToTripModalCtrl.js
@@ -2,31 +2,119 @@
 
 app.controller("AddToTripModalCtrl", function ($scope, TripFactory, $uibModalInstance){
 
+  //Get the Trips from a user's firebase and use them to populate the trips dropdown menu
   function getTrips (){
+    let trips = [];
     TripFactory.getTrips()
     .then((tripData)=>{
-      $scope.trips = tripData;
+      Object.keys(tripData).forEach((key)=>{
+        tripData[key].id = key;
+        trips.push(tripData[key]);
+      });
+      $scope.trips = trips;
     });
   }
 
   getTrips();
 
-  $scope.close = () => {
-    $uibModalInstance.close();
+  //We need to get available days from the trip chosen in the dropdown menu
+  $scope.getDaysAndLogTrip= (tripId)=>{
+    $scope.tripId = tripId;
+    console.log("trip id is: ", tripId);
+    TripFactory.getSingleTrip(tripId)
+    .then((tripData)=>{
+      $scope.trip = tripData;
+    });
   };
 
-  $scope.status = {
-    isopen: false
-  };
-
-  $scope.openTrips = ($event)=>{
-    $event.preventDefault();
-    $event.stopPropagation();
-    $scope.status.isopen = !$scope.status.isopen;
+  $scope.logDay = (dayId)=>{
+    console.log("day id", dayId);
   }
 
-  $scope.toggled = function(open) {
-    console.log('Dropdown is now: ', open);
+  $scope.status = {
+    isTripOpen: false,
+    isDayOpen: false,
+    isDayDisabled: true,
+    isTimeOpen: false
   };
 
+  //Opens the clicked dropdown menu
+  $scope.openTrips= ($event)=>{
+    $event.preventDefault();
+    $event.stopPropagation();
+    //Want to close the other dropdown menus before opening the chosen dropdown
+    $scope.status.isDayOpen = false;
+    $scope.status.isTimeOpen = false;
+    $scope.status.isTripOpen = !$scope.status.isTripOpen;
+  };
+
+  $scope.openDays= ($event)=>{
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.status.isTripOpen = false;
+    $scope.status.isTimeOpen = false;
+    $scope.status.isDayOpen = !$scope.status.isDayOpen;
+  };
+
+  $scope.openTime= ($event)=>{
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.status.isTripOpen = false;
+    $scope.status.isDayOpen = false;
+    $scope.status.isTimeOpen = !$scope.status.isTimeOpen;
+  };
+
+  //Closes the Trips dropdown
+  $scope.closeTrips = function() {
+    console.log("closing?", $scope.status.isTripOpen)
+    //not working
+    $scope.status.isTripOpen = false;
+    //working
+    $scope.status.isDayDisabled = false;
+  };
+
+  //Closes the days dropdown
+  $scope.closeDays = function(startDate) {
+    console.log("what is the start date?", startDate);
+    $scope.status.isDayOpen = false;
+    $scope.startTime = new Date(`${startDate} 7:00:00`);
+    $scope.endTime = new Date(`${startDate} 7:00:00`);
+  };
+
+  // $scope.closeTime = function() {
+  //   $scope.status.isTimeOpen = false;
+  // };
+
+  //Closes Modal (nothing is saved)
+  $scope.closeModal = () => {
+    $uibModalInstance.close();
+    };
+
+
+  $scope.hstep = 1;
+  $scope.mstep = 15;
+
+  $scope.ismeridian = true;
+  $scope.toggleMode = function() {
+    $scope.ismeridian = ! $scope.ismeridian;
+  };
+
+  $scope.updateStart = function() {
+    console.log("new start time", $scope.startTime);
+  };
+
+  $scope.updateEnd = function() {
+    console.log("new end time", $scope.endTime);
+  };
+
+  $scope.changed = function () {
+    console.log('Time changed to: ' + $scope.startTime);
+    console.log('Time changed to: ' + $scope.endTime);
+  };
+
+  //  $scope.toggleDropdown = function($event) {
+  //   $event.preventDefault();
+  //   $event.stopPropagation();
+  //   $scope.status.isopen = !$scope.status.isopen;
+  // };
 });
--- app/controllers/CreateTripModalCtrl.js
+++ app/controllers/CreateTripModalCtrl.js
@@ -2,17 +2,40 @@
 
 app.controller('CreateTripModalCtrl', function($scope, $uibModalInstance, AuthFactory, TripFactory, $route, startDate, endDate, isEditing, $mdDateLocale) {
 
+  //Convert the dates passed into the controller function to MM-DD-YYYY format so that they are in a nice format for the modal view.
   $mdDateLocale.formatDate = function(date) {
     return moment(date).format('MM-DD-YYYY');
   };
-
-  let startMilliseconds = startDate.getTime();
-  let endMilliseconds = endDate.getTime();
-
   $scope.startDt = $mdDateLocale.formatDate(startDate);
   $scope.endDt = $mdDateLocale.formatDate(endDate);
   $scope.isEditing = isEditing;
 
+  //The trip object that is saved to firebase should have an array of days so that trips can be added to specific days of the trip
+  let startMilliseconds = startDate.getTime();
+  let endMilliseconds = endDate.getTime();
+  //There are 86400000 milliseconds in a day
+  let oneDay = 86400000;
+
+  function getNumberOfDays (){
+    let difference = endMilliseconds - startMilliseconds;
+    return (Math.round(difference/oneDay)+1);
+  };
+
+  //For each day of the trip I want to have a day number, corresponding date, and id. This uses the number of days and creates an array of objects with the desired properties.
+  function createDayArray (){
+    let days = [];
+    for (let i=0; i<getNumberOfDays(); i++){
+      days.push({
+        day: 'Day ' + (i+1),
+        date: moment(startMilliseconds + (i*oneDay)).format("dddd MMMM Do YYYY"),
+        startDate,
+        id: i
+      });
+    };
+    return days;
+  };
+
+  //The following are the properties on the trip object saved to firebase
   $scope.trip = {
     name: '',
     description: '',
@@ -20,13 +43,12 @@ app.controller('CreateTripModalCtrl', function($scope, $uibModalInstance, AuthFa
     endDate,
     startMilliseconds,
     endMilliseconds,
-    imgUrl: ''
+    imgUrl: '',
+    numberOfDays: getNumberOfDays(),
+    days: createDayArray()
   }
 
-  $scope.close = () => {
-    $uibModalInstance.close();
-  };
-
+  //A trip is created and added to firebase using the create button 
   $scope.create = () => {
     $scope.trip.uid = AuthFactory.getUserId();
     TripFactory.createTrip($scope.trip)
@@ -35,6 +57,11 @@ app.controller('CreateTripModalCtrl', function($scope, $uibModalInstance, AuthFa
       $uibModalInstance.close();
       $route.reload();
     });
+  };
+
+  //Closes the modal
+  $scope.close = () => {
+    $uibModalInstance.close();
   };
 
   // $scope.update = (board)=>{
