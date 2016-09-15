26ca78e97f14dd3d904e78e16acaad5e5a867f38
Delaine Wendling <delaine.wendling@gmail.com>
2016-09-15 15:04:24 -0500

    Started setting up UI Route to route within a partial

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var app = angular.module("NPApp", ["ngRoute", "uiGmapgoogle-maps", 'ngMaterial', 'ngMessages', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'dndLists'])
+var app = angular.module("NPApp", ["ngRoute", "uiGmapgoogle-maps", 'ngMaterial', 'ngMessages', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'dndLists', 'ui.router'])
 .constant("FirebaseURL", "https://national-parks-trip-planner.firebaseio.com/")
 .config(function(uiGmapGoogleMapApiProvider, ImportantKeys) {
   uiGmapGoogleMapApiProvider.configure({
@@ -36,38 +36,85 @@ let isAuth = (AuthFactory, $window)=> new Promise((resolve, reject)=>{
       reject();
     }
 });
-app.config(function($routeProvider){
-  $routeProvider
-  .when('/', {
-    templateUrl: 'partials/Login.html',
-    controller: 'LoginCtrl'
-  })
-  .when('/login', {
-    templateUrl: 'partials/Login.html',
-    controller: 'LoginCtrl'
-  })
-  .when('/parks/explore', {
-    templateUrl: 'partials/Explore.html',
-    controller: 'ExploreCtrl',
-    resolve: {isAuth}
-  })
-  .when('/parks/trips/create', {
-    templateUrl: 'partials/CreateTripModal.html',
-    controller: 'CreateTripModalCtrl',
-    resolve: {isAuth}
-  })
-  .when('/parks/trip/:tripId', {
-    templateUrl: 'partials/SingleTrip.html',
-    controller: 'SingleTripCtrl',
-    resolve: {isAuth}
-  })
-  .when('/parks/wishlist', {
-    templateUrl: 'partials/Wishlist.html',
-    controller: 'WishlistCtrl',
-    resolve: {isAuth}
-  })
-  .otherwise('/');
-});
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        // HOME STATES ========================================
+        .state('/', {
+            url: '/',
+            templateUrl: 'partials/Login.html',
+            controller: 'LoginCtrl'
+        })
+        .state('/login', {
+            url: '/login',
+            templateUrl: 'partials/Login.html',
+            controller: 'LoginCtrl'
+        })
+        .state('/parks/explore', {
+            url: '/parks/explore',
+            templateUrl: 'partials/Explore.html',
+            controller: 'exploreCtrl',
+            resolve: {isAuth}
+        })
+        .state('/parks/trips/create', {
+            url: '/parks/trips/create',
+            templateUrl: 'partials/CreateTripModal.html',
+            controller: 'CreateTripModalCtrl',
+            resolve: {isAuth}
+        })
+        .state('/parks/trip/:tripId', {
+            templateUrl: 'partials/SingleTrip.html',
+            controller: 'SingleTripCtrl',
+            resolve: {isAuth}
+        })
+        .state('/parks/trip/:tripId.averageTemp', {
+        url: '/list',
+        templateUrl: 'partial-home-list.html',
+        controller: function($scope) {
+            $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+          }
+        })
+        .when('/parks/wishlist', {
+            templateUrl: 'partials/Wishlist.html',
+            controller: 'WishlistCtrl',
+            resolve: {isAuth}
+        })
+  });
+// app.config(function($routeProvider){
+//   $routeProvider
+//   .when('/', {
+//     templateUrl: 'partials/Login.html',
+//     controller: 'LoginCtrl'
+//   })
+//   .when('/login', {
+//     templateUrl: 'partials/Login.html',
+//     controller: 'LoginCtrl'
+//   })
+//   .when('/parks/explore', {
+//     templateUrl: 'partials/Explore.html',
+//     controller: 'ExploreCtrl',
+//     resolve: {isAuth}
+//   })
+//   .when('/parks/trips/create', {
+//     templateUrl: 'partials/CreateTripModal.html',
+//     controller: 'CreateTripModalCtrl',
+//     resolve: {isAuth}
+//   })
+//   .when('/parks/trip/:tripId', {
+//     templateUrl: 'partials/SingleTrip.html',
+//     controller: 'SingleTripCtrl',
+//     resolve: {isAuth}
+//   })
+//   .when('/parks/wishlist', {
+//     templateUrl: 'partials/Wishlist.html',
+//     controller: 'WishlistCtrl',
+//     resolve: {isAuth}
+//   })
+//   .otherwise('/');
+// });
 
 //Initializes Firebase right away
 app.run((ImportantKeys)=>{
