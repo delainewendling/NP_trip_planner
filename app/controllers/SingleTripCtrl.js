4eb0f9aef2554421e182e6290263b129f4588021
Delaine Wendling <delaine.wendling@gmail.com>
2016-09-15 10:06:04 -0500

    Allow the users to add and update notes in the trip view

--- app/controllers/SingleTripCtrl.js
+++ app/controllers/SingleTripCtrl.js
@@ -2,19 +2,16 @@
 
 app.controller('SingleTripCtrl', function($scope, $routeParams, TripFactory, TrailFactory, $route, NoteFactory, $uibModal){
 
-  //Need to define these things. Activities will have a trip Id on them that matches the routeParams
-  // $scope.activities
-  //Write a function in the TripFactory that gets the correct single trip from the database
-
-  $scope.activities = [];
+  //This is where the trails that have been added to this trip are located. 
+  $scope.trails = [];
 
     TripFactory.getSingleTrip($routeParams.tripId)
     .then((tripData)=>{
-      //trip.day.id is correct here
       console.log("trip data", tripData);
       $scope.trip = tripData;
     });
 
+    //Opens a modal to make sure the user really wants to delete the trip
     $scope.openDeleteModal = ()=>{
       let modalInstance = $uibModal.open({
       templateUrl: 'partials/DeleteTrip.html',
@@ -22,32 +19,32 @@ app.controller('SingleTripCtrl', function($scope, $routeParams, TripFactory, Tra
       });
     };
 
-    // showNotes();
+    showNotes();
     showTrails();
+    $scope.notes = [];
 
     function showNotes(){
       NoteFactory.getNotes($routeParams.tripId)
       .then((noteData)=>{
-        let notes = [];
         console.log("note data", noteData);
         Object.keys(noteData).forEach((key)=>{
           noteData[key].id =key;
-          notes.push(noteData[key]);
+          $scope.notes.push(noteData[key]);
         });
-        addNotesToActivities(notes);
+        addNotesToNotes($scope.notes);
       });
     };
 
-    function addNotesToActivities(notes){
+    function addNotesToNotes(notes){
       Object.keys(notes).forEach((key)=>{
-        $scope.activities.push({
+        $scope.notes.push({
           text: notes[key].text,
-          dayId: notes[key].dayId,
+          day: notes[key].dayId,
           id: notes[key].id,
           wishlist: false
         });
       });
-      console.log("activities", $scope.activities);
+      console.log("notes", $scope.notes);
     };
 
     function showTrails(){
@@ -58,14 +55,14 @@ app.controller('SingleTripCtrl', function($scope, $routeParams, TripFactory, Tra
           trailData[key].id =key;
           trails.push(trailData[key]);
         })
-        addTrailsToActivities(trails);
+        addTrailsToTrailsArr(trails);
       });
     }
 
-    function addTrailsToActivities(trails){
+    function addTrailsToTrailsArr(trails){
       console.log("trails", trails);
       Object.keys(trails).forEach((key)=>{
-        $scope.activities.push({
+        $scope.trails.push({
           name: trails[key].name,
           dayId: trails[key].dayId,
           id: trails[key].id,
@@ -79,42 +76,55 @@ app.controller('SingleTripCtrl', function($scope, $routeParams, TripFactory, Tra
           wishlist: true
         });
       });
-      console.log("activities", $scope.activities);
+      console.log("activities", $scope.trails);
     };
 
     $scope.deleteTrailFromTrip = (trailId)=>{
       TripFactory.deleteTrailFromTrip(trailId)
       .then(()=>{
         console.log("successfully deleted");
-        showTrails();
+        //For now, the only way I know how to get rid of the deleted item is to reload the page. Not ideal - want to fix later. 
+        $route.reload();
+        // showTrails();
       })
     }
 
-    $scope.cardAdded = false;
-    $scope.createNote= (dayId)=>{
-      $scope.activities.push({
+    $scope.addNote= (dayId)=>{
+      //Need to create a noteObj to add to Firebase. Even though there is no text in this note the note instance needs to be added so that the user is updating the note when pressing enter rather than creating a whole new note everytime the enter key is pressed.
+      let noteObj = {
         text: '',
-        id: dayId,
-        wishlist: false
+        day: dayId,
+        tripId: $routeParams.tripId
+      }
+      //A new note needs to be added to $scope.notes so that the user sees a card show up on the screen. This instance of the note will be replaced with the note in firebase when the user navigates away from this page.
+      $scope.notes.push(noteObj);
+      //The note is added to firebase
+      NoteFactory.addNote(noteObj)
+      .then((note)=>{
+        console.log("added note!", note);
       });
-      $scope.cardAdded = true;
-      console.log("activities?", $scope.activities);
-    }
+    };
 
-    $scope.addNote = (event, dayId, text)=>{
+    $scope.updateNote = (event, noteId, text)=>{
       if (event.charCode == 13) {
-        console.log("note will be added", dayId);
-        let noteObj = {
-          text,
-          day: dayId,
-          tripId: $routeParams.tripId
-        }
-        NoteFactory.addNote(noteObj)
+        console.log("note will be updated", noteId);
+        let textPatch = { text }
+        NoteFactory.updateNote(textPatch, noteId)
         .then((note)=>{
-          console.log("added note!");
+          console.log("updated note!");
         });
       };
     };
+
+    // $scope.autoExpand = function(e) {
+    //     var element = typeof e === 'object' ? e.target : document.getElementById(e);
+    //     var scrollHeight = element.scrollHeight -60; // replace 60 by the sum of padding-top and padding-bottom
+    //     element.style.height =  scrollHeight + "px";
+    // };
+
+    // function expand() {
+    //   $scope.autoExpand('TextArea');
+    // }
 
 });
 
--- app/factories/NoteFactory.js
+++ app/factories/NoteFactory.js
@@ -4,8 +4,9 @@ app.factory('NoteFactory', ($q, $http, FirebaseURL)=>{
 
   let getNotes = (tripId)=>{
     return $q((resolve, reject)=>{
-      $http.get(`${FirebaseURL}/notes.json?orderBy="tripId"&equalTo="${tripId}`)
+      $http.get(`${FirebaseURL}notes.json?orderBy="tripId"&equalTo="${tripId}"`)
       .success((noteData)=>{
+        console.log("notes from firebase?", noteData);
         resolve(noteData);
       })
       .error((error)=>{
@@ -16,7 +17,7 @@ app.factory('NoteFactory', ($q, $http, FirebaseURL)=>{
 
   let addNote = (noteObj)=>{
     return $q((resolve, reject)=>{
-      $http.get(`${FirebaseURL}/notes.json`, JSON.stringify(noteObj))
+      $http.post(`${FirebaseURL}notes.json`, JSON.stringify(noteObj))
       .success((noteData)=>{
         resolve(noteData);
       })
@@ -26,5 +27,17 @@ app.factory('NoteFactory', ($q, $http, FirebaseURL)=>{
     });
   };
 
-  return {getNotes, addNote};
+  let updateNote = (patchedText, noteId)=>{
+    return $q((resolve, reject)=>{
+      $http.patch(`${FirebaseURL}notes/${noteId}.json`, JSON.stringify(patchedText))
+      .success((newNote)=>{
+        resolve(newNote);
+      })
+      .error((error)=>{
+        reject(noteData);
+      });
+    });
+  };
+
+  return {getNotes, addNote, updateNote};
 });
