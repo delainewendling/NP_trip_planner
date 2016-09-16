d2f981bad81be7073ecdf8ea6b1dc59a9ef6baad
Delaine Wendling <delaine.wendling@gmail.com>
2016-09-16 09:18:19 -0500

    Fixed a bug that popped up in the average temperature modal

--- app/controllers/AverageTempModalCtrl.js
+++ app/controllers/AverageTempModalCtrl.js
@@ -18,6 +18,7 @@ app.controller('AverageTempModalCtrl', function($scope, $uibModalInstance, TripF
 
   function tripMonths (){
     if (startMonthId === endMonthId){
+      console.log("same month", startMonthId, endMonthId);
       getAverageTempForMonth(startMonthId);
     } else {
       console.log("not the same start and end month")
@@ -25,16 +26,18 @@ app.controller('AverageTempModalCtrl', function($scope, $uibModalInstance, TripF
       var endMonth = parseInt(endMonthId,10);
       if (startMonth < endMonth){
         for (var i=startMonth; i<=endMonth; i++){
-          let monthId = (i < 10 ? "0" : "") + i;
+          //Need to convert the numbers back to strings to make a call to firebase
+          let monthId = ("") + i;
           getAverageTempForMonth(monthId);
         }
       } else {
+        //If the start month number is larger than the end month number then I need to loop through the months before December and then loop the months after January.
         for (var i=startMonth; i<=12; i++){
-          let monthId = (i < 10 ? "0" : "") + i;
+          let monthId = ("") + i;
           getAverageTempForMonth(monthId);
         }
         for (var i=1; i<=endMonth; i++){
-          let monthId = (i < 10 ? "0" : "") + i;
+          let monthId = ("") + i;
           getAverageTempForMonth(monthId);
         }
       }
@@ -44,17 +47,13 @@ app.controller('AverageTempModalCtrl', function($scope, $uibModalInstance, TripF
   function getAverageTempForMonth (monthId){
     TripFactory.getAverageTemp(monthId)
     .then((tempData)=>{
-      console.log("tempData", tempData);
+      //tempData is an object within an object. In order to access the weather information one needs to go into the object within the tempData object. The Firebase key is the property needed to access the inner object to I am creating an array of keys and an array of objects and the 0 index of one matches the 0 index of the other. In the partial I use the index and key to access the correct information.
       Object.keys(tempData).forEach((key)=>{
         $scope.keys.push(key);
       });
       $scope.months.push(tempData);
-      console.log("months array", $scope.months);
     });
   }
-
-  // TripFactory.getAverageTemp(month)
-
 
 });
 
